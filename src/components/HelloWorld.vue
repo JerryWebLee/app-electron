95c5b04b3cffa60932a1bba8363ff8e5af6340e7
JerryWebLee <jerryweblee@outlook.com>
2025-11-24 22:18:20 +0800

    feat: Initialize Electron-Vue 3 project with Vite, Pinia, Vue Router, and Element Plus.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useMainStore } from '../store'
+import { storeToRefs } from 'pinia'
+
+const store = useMainStore()
+const { count } = storeToRefs(store)
+</script>
+
+<template>
+  <div class="card">
+    <h1>Electron + Vue 3 + Element Plus</h1>
+    <el-button type="primary" @click="store.increment">Count is {{ count }}</el-button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
